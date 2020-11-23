ce14d6702c1a30b0b4bd8711b488d1e015e0bb55
Raild3x <47638612+Raild3x@users.noreply.github.com>
2020-11-23 15:31:36 -0500

    test

--- JS/jsHandler.js
+++ JS/jsHandler.js
@@ -73,6 +73,8 @@ function newElement(tblName, fields) {
             var columns = JSON.parse(this.response);
             for (col in columns) {
                 col = columns[col];
+                col.replace(/([A-Z])/g, ' $1').trim();
+                col = col.charAt(0).toUpperCase() + col.slice(1);
                 fields.innerHTML = fields.innerHTML + "<label class='fieldLabel'>" + col + ": </label><input type='text' id='"+col+"Field'></br>";
             }
             
